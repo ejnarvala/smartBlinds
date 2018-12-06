7bcfa49d30e7b98c167c06bef2004605929e4ad1
Ejnar Arechavala <ejnar123@gmail.com>
2018-12-05 21:41:22 -0500

    updated tabs

--- smartblinds.py
+++ smartblinds.py
@@ -3,9 +3,9 @@ from imu import *
 app = Flask(__name__)
 @app.route('/')
 def index():
-	if(lib.lsm9ds1_gyroAvailable(imu) == 0):
-		return 'IMU Unavailable'
-	else:
+    if(lib.lsm9ds1_gyroAvailable(imu) == 0):
+        return 'IMU Unavailable'
+    else:
         lib.lsm9ds1_readGyro(imu)
         gx = lib.lsm9ds1_getGyroX(imu)
         gy = lib.lsm9ds1_getGyroY(imu)
@@ -14,11 +14,11 @@ def index():
         cgx = lib.lsm9ds1_calcGyro(imu, gx)
         cgy = lib.lsm9ds1_calcGyro(imu, gy)
         cgz = lib.lsm9ds1_calcGyro(imu, gz)
-    	return 'X: ' + str(cgx) + ',Y: ' + str(cgy) + ',Z: ' + str(cgz)
+        return 'X: ' + str(cgx) + ',Y: ' + str(cgy) + ',Z: ' + str(cgz)
 
 if __name__ == '__main__':
-	lib = startIMU()
-	imu = lib.lsm9ds1_create()
+    lib = startIMU()
+    imu = lib.lsm9ds1_create()
     lib.lsm9ds1_begin(imu)
     if lib.lsm9ds1_begin(imu) == 0:
         print("Failed to communicate with LSM9DS1.")
@@ -41,8 +41,3 @@ if __name__ == '__main__':
 
         # print("Gyro: %f, %f, %f [deg/s]" % (cgx, cgy, cgz))
     app.run(debug=True, host='0.0.0.0', port=80)
-
-
-
-
-
