13595fb4fe55ab045a27189fbb9a15f16df8daf3
Ejnar Arechavala <ejnar123@gmail.com>
2018-12-05 16:24:53 -1000

    added library

--- imu.py
+++ imu.py
@@ -4,7 +4,7 @@
 #import ctypes
 from ctypes import *
 import time
-path = "../lib/liblsm9ds1cwrapper.so"
+path = "./lib/liblsm9ds1cwrapper.so"
 lib = cdll.LoadLibrary(path)
 
 lib.lsm9ds1_create.argtypes = []
